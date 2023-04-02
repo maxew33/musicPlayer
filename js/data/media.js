83b8548dc061fc4a41b0ef0951c52874be99b918
Maxime Malfilâtre <maxime.malfilatre@gmail.com>
2023-04-02 09:32:18 +0200

    :sparkles: change controllers UI
    place the controllers on the disc

--- js/data/media.js
+++ js/data/media.js
@@ -1,210 +1,258 @@
 export const media = {
     "jazz": [
         {
-            "artist": "",
-            "title": ""
+            "artist": "TrackTribe",
+            "title": "Walk Through the Park",
+            "path": "WalkThroughthePark-TrackTribe.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "TrackTribe",
+            "title": "Home for the Holidays",
+            "path": "HomefortheHolidays-TrackTribe.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "TrackTribe",
+            "title": "Hedge Your Bets",
+            "path": "HedgeYourBets-TrackTribe.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "E's Jammy Jams",
+            "title": "Happy Birthday Jazz Waltz",
+            "path": "Happy_Birthday_Jazz_Waltz-E_s_Jammy_Jams.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "E's Jammy Jams",
+            "title": "Happy Birthday Mambo",
+            "path": "Happy_Birthday_Mambo-E_s_Jammy_Jams.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "E's Jammy Jams",
+            "title": "Minor Blues for Booker",
+            "path": "Minor_Blues_for_Booker-E_s_Jammy_Jams.mp3"
         }
     ],
     "pop": [
         {
-            "artist": "",
-            "title": ""
+            "artist": "Patrick Patrikios",
+            "title": "Know Myself",
+            "path": "KnowMyself-PatrickPatrikios.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Patrick Patrikios",
+            "title": "Where We Wanna Go",
+            "path": "WhereWeWannaGo-PatrickPatrikios.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Patrick Patrikios",
+            "title": "You Should",
+            "path": "YouShould-PatrickPatrikios.mp3"
+        }, 
+        {
+            "artist": "Anno Domini Beats",
+            "title": "Glass",
+            "path": "Glass-AnnoDominiBeats.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Anno Domini Beats",
+            "title": "Pray",
+            "path": "Pray-AnnoDominiBeats.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Anno Domini Beats",
+            "title": "Skylines",
+            "path": "Skylines-AnnaDominiBeats.mp3"
         },
-        {
-            "artist": "",
-            "title": ""
-        }
     ],
     "rnb": [
         {
-            "artist": "",
-            "title": ""
+            "artist": "Bad Snacks",
+            "title": "Easy Saturday",
+            "path": "EasySaturday-BadSnacks.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Bad Snacks",
+            "title": "Equinox",
+            "path": "Equinox-BadSnacks.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Bad Snacks",
+            "title": "Honey",
+            "path": "Honey-BadSnacks.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Otis McDonald",
+            "title": "Forever Young",
+            "path": "ForeverYoung-OtisMcDonald.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Otis McDonald",
+            "title": "I Wish I Knew",
+            "path": "IWishIKnew-OtisMcDonald.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Otis McDonald",
+            "title": "Spooky Boop",
+            "path": "SpookyBoop-OtisMcDonald.mp3"
         }
     ],
     "reggae": [
         {
-            "artist": "",
-            "title": ""
+            "artist": "Freedom Trail Studio",
+            "title": "General One",
+            "path": "GeneralOne-FreedomTrailStudio.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Freedom Trail Studio",
+            "title": "Palm Avenue",
+            "path": "PalmAvenue-FreedomTrailStudio.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Freedom Trail Studio",
+            "title": "Steppers To Heavan",
+            "path": "SteppersToHeavan-FreedomTrailStudio.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Nana Kwabena",
+            "title": "Ice Kenkey Beat",
+            "path": "IceKenkeyBeat-NanaKwabena.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Nana Kwabena",
+            "title": "Revenge Body Beat",
+            "path": "RevengeBodyBeat-NanaKwabena.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Nana Kwabena",
+            "title": "She No Dull Beat",
+            "path": "SheNoDullBeat-NanaKwabena.mp3"
         }
     ],
     "rock": [
         {
-            "artist": "",
-            "title": ""
+            "artist": "RKVC",
+            "title": "Burlesque Heartache",
+            "path": "BurlesqueHeartache-RKVC.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "RKVC",
+            "title": "Outta Time",
+            "path": "OuttaTime-RKVC.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "RKVC",
+            "title": "Tropical Thunder",
+            "path": "TropicalThunder-RKVC.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Jeremy Korpas",
+            "title": "Emergency On Level 3",
+            "path": "EmergencyOnLevel3-JeremyKorpas.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Jeremy Korpas",
+            "title": "Sharp",
+            "path": "Sharp-JeremyKorpas.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Jeremy Korpas",
+            "title": "Twin Engines",
+            "path": "TwinEngines-JeremyKorpas.mp3"
         }
     ],
     "country": [
         {
-            "artist": "",
-            "title": ""
+            "artist": "Chris Haugen",
+            "title": "Birdseye Blues",
+            "path": "BirdseyeBlues-ChrisHaugen.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Chris Haugen",
+            "title": "Hulu Ukulele",
+            "path": "HuluUkulele-ChrisHaugen.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Chris Haugen",
+            "title": "Si Señorita",
+            "path": "SiSeñorita-ChrisHaugen.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Cooper Cannell",
+            "title": "Amazing Grace",
+            "path": "AmazingGrace-CooperCannell.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Cooper Cannell",
+            "title": "Muffin Man",
+            "path": "MuffinMan-CooperCannell.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Cooper Cannell",
+            "title": "Twinkle Twinkle Little Star",
+            "path": "TwinkleTwinkleLittleStar-CooperCannell.mp3"
         }
     ],
     "classic": [
         {
-            "artist": "",
-            "title": ""
+            "artist": "Asher Fulero",
+            "title": "Ceremonial Library",
+            "path": "CeremonialLibrary-AsherFulero.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Asher Fulero",
+            "title": "Kiss The Heavens",
+            "path": "KissTheHeavens-AsherFulero.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Asher Fulero",
+            "title": "Snowfall Butterflies",
+            "path": "SnowfallButterflies-AsherFulero.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Joel Cummins",
+            "title": "Sonatina No 2 in F Major Allegro",
+            "path": "SonatinaNo2inFMajorAllegro-JoelCummins.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Joel Cummins",
+            "title": "Sarabande",
+            "path": "Sarabande-JoelCummins.mp3"
         },
         {
-            "artist": "",
-            "title": ""
+            "artist": "Joel Cummins",
+            "title": "Pastorale",
+            "path": "Pastorale-JoelCummins.mp3"
         }
     ],
     "punk": [
         {
-            "artist": "Vans_in_Japan",
-            "title": "Grey_Flannel"
+            "artist": "Vans in Japan",
+            "title": "Grey Flannel",
+            "path": "Grey_Flannel-Vans_in_Japan.mp3"
         },
         {
-            "artist": "Vans_in_Japan",
-            "title": "Tuff_Data"
+            "artist": "Vans in Japan",
+            "title": "Tuff Data",
+            "path": "Tuff_Data-Vans_in_Japan.mp3"
         },
         {
-            "artist": "Vans_in_Japan",
-            "title": "Desert_Brawl"
-        },
-        {
-            "artist": "NEFFEX",
-            "title": "6_Shots"
-        },
-        {
-            "artist": "NEFFEX",
-            "title": "Tough"
+            "artist": "Vans in Japan",
+            "title": "Desert Brawl",
+            "path": "Desert_Brawl-Vans_in_Japan.mp3"
         },
         {
             "artist": "NEFFEX",
-            "title": "No_Filter"
+            "title": "6 Shots",
+            "path": "6_Shots-NEFFEX.mp3"
+        },
+        {
+            "artist": "NEFFEX",
+            "title": "Tough",
+            "path": "Tough-NEFFEX.mp3"
+        },
+        {
+            "artist": "NEFFEX",
+            "title": "No Filter",
+            "path": "No_Filter-NEFFEX.mp3"
         }
     ]
 }
--- js/script.js
+++ js/script.js
@@ -1,98 +1,94 @@
 import { media } from "./data/media.js"
 import { getRndNumber } from "./utils/functions.js"
 
-const cards = [...document.querySelectorAll('.card')],
+const discs = [...document.querySelectorAll('.disc')],
     library = document.querySelector('.disc-library'),
     arrows = [...document.querySelectorAll('.arrow')],
-    player = [...document.querySelectorAll('[aria-hidden]')],
+    player = [...document.querySelectorAll('.player-elt')],
     exitPlayer = document.querySelector('.exit'),
-    playlist = document.querySelector('.playlist'),
     mediaInfos = [],
-    soundtracks = [],
-    audio = new Audio
+    audio = new Audio,
+    trackInfos = document.querySelector('.track-infos')
 
-let angle = 0,
-    chgtCard = 0,
+let chgtdisc = 0,
     canChange = true, //prevent spam
     prevX,
     prevY,
     timeElapsed = 50,
     genreSelected,
     genreWrapper,
-    play = false,
+    pause = false,
     trackIndex = 0,
-    random = false
+    random = false,
+    albumOpened = false
 
 
-window.onload = animateCards()
+window.onload = animatediscs()
 
-// first card animation when the page is loaded
-function animateCards() {
-    cards.forEach((card, idx) => {
+// first disc animation when the page is loaded
+function animatediscs() {
+    discs.forEach((disc, idx) => {
         setTimeout(function () {
 
             let divClassName
 
             if (idx % 2 === 0) {
-                divClassName = "card-from-left"
+                divClassName = "disc-from-left"
             }
             else {
-                divClassName = "card-from-right"
+                divClassName = "disc-from-right"
             }
 
-            card.classList.remove(divClassName)
-            setTimeout(function () {
-                card.classList.add("card-centered")
-            }, 150 + cards.length * 150)
+            disc.classList.remove(divClassName)
+
         }, timeElapsed)
 
         timeElapsed += 150
     })
 }
 
-function changeCard(direction) {
+function changedisc(direction) {
     // direction : -1 => left / 1 => right
 
     canChange = false
 
-    cards.forEach(card => {
+    discs.forEach(disc => {
 
-        let pos = + card.dataset.pos
+        let pos = + disc.dataset.pos
         let newPos = pos + direction
 
-        card.dataset.pos = newPos
+        disc.dataset.pos = newPos
 
         if (newPos === 0) {
             setTimeout(function () {
-                card.dataset.pos = 8
+                disc.dataset.pos = 8
                 canChange = true
             }, 500)
         }
         else if (newPos === 9) {
             setTimeout(function () {
-                card.dataset.pos = 1
+                disc.dataset.pos = 1
                 canChange = true
             }, 500)
         }
     })
 }
 
-
-// -A- change cards with wheel
+// -A- change discs with wheel
 library.addEventListener('wheel', e => {
 
     if (canChange) {
         const direction = Math.sign(e.deltaY)
-        chgtCard += direction
+        chgtdisc += direction
 
-        Math.abs(chgtCard) > 5 && (
-            changeCard(direction),
-            chgtCard = 0)
+        Math.abs(chgtdisc) > 5 && (
+            changedisc(direction),
+            chgtdisc = 0)
     }
 })
 
 
-// -B- change cards with touches
+// -B- change discs with touches
 library.addEventListener('touchstart', (e) => {
     prevX = e.touches[0].clientX
     prevY = e.touches[0].clientY
@@ -111,66 +107,52 @@ library.addEventListener('touchmove', (e) => {
                 prevX = newX)
                 : (direction = Math.sign(newY - prevY),
                     prevY = newY)
-            changeCard(direction)
+            changedisc(direction)
         }
     }
 })
 
-// -C- change cards with arrows
-arrows.forEach(arrow => arrow.addEventListener('click', e => canChange && changeCard(parseInt(e.target.dataset.dir))))
+// -C- change discs with arrows
+arrows.forEach(arrow => arrow.addEventListener('click', e => canChange && changedisc(parseInt(e.target.dataset.dir))))
 
 
 // Part II - select an album
-cards.forEach(card => card.addEventListener('click', e => {
-    // cards.forEach(card => card !== genreSelected && (card.style.opacity = "0"))
-    console.log('click on', e.target.dataset.genre)
-    // aria selected = true
+discs.forEach(disc => disc.addEventListener('click', e => {
+
+    albumOpened ? closeAlbum() : openAlbum(e)
+    
+}))
+
+exitPlayer.addEventListener('click', closeAlbum)
+
+function openAlbum(e){
+    
     genreWrapper = e.target
     genreSelected = e.target.dataset.genre
 
     mediaInfos.length = 0
     mediaInfos.push(...media[genreSelected])
 
-    playlist.innerHTML = ''
-
-    mediaInfos.forEach((media, idx) => {
-        playlist.innerHTML += `
-        <div class="soundtrack" aria-selected="false">
-        <div>
-        ${idx + 1} 
-        </div>
-        artist: ${media.artist}
-        <div>
-        </div>
-        title: ${media.title}
-        <div>
-        </div>
-        <div>
-        `
-    })
-
-    soundtracks.length = 0
-    soundtracks.push(...Array.from(document.querySelectorAll('.soundtrack')))
-
-    soundtracks.forEach((track, idx) => track.addEventListener('click', () => {
-        trackIndex = idx
-        playMusik()
-    }))
-
     genreWrapper.setAttribute('aria-selected', true)
     player.forEach(elt => elt.setAttribute("aria-hidden", false))
     setTimeout(() => { playMusik() }, 500)
-}))
 
-exitPlayer.addEventListener('click', () => {
+    albumOpened = true
+}
+
+function closeAlbum(){
     setTimeout(() => {
         genreWrapper.setAttribute('aria-selected', false)
     }, 500)
 
     player.forEach(elt => elt.setAttribute("aria-hidden", true))
 
+    trackInfos.innerText=''
+
     stopMusik()
-})
+
+    albumOpened = false
+}
 
 
 //Part III Music player
@@ -178,9 +160,10 @@ const playBtn = document.querySelector('.play'),
     changeTrackBtn = Array.from(document.querySelectorAll('.change-track')),
     pauseBtn = document.querySelector('.pause'),
     stopBtn = document.querySelector('.stop'),
-    randomBtn = document.querySelector('.random')
+    randomBtn = document.querySelector('.random'),
+    setVolumeBtn = Array.from(document.querySelectorAll('.set-volume'))
 
-playBtn.addEventListener('click', !play && playMusik)
+playBtn.addEventListener('click', pauseMusik)
 pauseBtn.addEventListener('click', pauseMusik)
 stopBtn.addEventListener('click', stopMusik)
 
@@ -188,26 +171,43 @@ randomBtn.addEventListener('click', () => random = !random)
 
 changeTrackBtn.forEach(btn => btn.addEventListener('click', e => changeTrack(+e.target.dataset.value)))
 
-function playMusik() {
-    soundtracks.forEach(track => track.setAttribute('aria-selected', false))
-    soundtracks[trackIndex].setAttribute('aria-selected', true)
+setVolumeBtn.forEach(btn => btn.addEventListener('click', e => setVolume(+e.target.dataset.value)))
 
-    audio.src = `../assets/music/${genreSelected}/${mediaInfos[trackIndex].title}-${mediaInfos[trackIndex].artist}.mp3`
+function playMusik() {
+
+    audio.src = `../assets/music/${genreSelected}/${mediaInfos[trackIndex].path}`
     audio.play()
+
+    trackInfos.innerText = `track ${trackIndex+1} - Artist: ${mediaInfos[trackIndex].artist} - title: ${mediaInfos[trackIndex].title}`
+
+    pause = false
+    displayPlayBtn()
 }
 
 function pauseMusik() {
-    pause ? audio.pause() : audio.play()
+
+    pause ? audio.play() : audio.pause()
     pause = !pause
+
+    displayPlayBtn()
+}
+
+function displayPlayBtn() {
+    pauseBtn.style.display = pause ? 'none' : 'block'
+    playBtn.style.display = pause ? 'block' : 'none'
 }
 
 function stopMusik() {
     audio.pause()
     audio.currentTime = 0
     trackIndex = 0
+    pause = true
+    
+    displayPlayBtn()
 }
 
 function changeTrack(value) {
+    console.log(value)
     if (random) {
         pickNextRndTrack()
     }
@@ -226,4 +226,12 @@ function pickNextRndTrack() {
     do { rndTrack = getRndNumber(mediaInfos.length) }
     while (rndTrack === trackIndex)
     trackIndex = rndTrack
+}
+
+function setVolume(value){
+    let currentVolume = audio.volume
+    currentVolume += value*0.1
+    currentVolume < 0 && (currentVolume = 0)
+    currentVolume > 1 && (currentVolume = 1)
+    audio.volume = currentVolume
 }
